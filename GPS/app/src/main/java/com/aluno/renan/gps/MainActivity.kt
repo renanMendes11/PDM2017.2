df476ec75b9ff30376cbda63b5d3d3bebc96c9b4
Renan <renanmendes049@gmail.com>
2017-11-28 22:49:47 -0300

    testando GPS

---
+++ GPS/app/src/main/java/com/aluno/renan/gps/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.aluno.renan.gps
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.ListView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var lvCoordenadas: ListView
+    lateinit var gpsManager: LocationManager
+    lateinit var gpsListener: LocationListener
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        this.lvCoordenadas = findViewById(R.id.lvCoordenadas)
+
+
+
+        this.gpsManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        this.gpsListener = GPSListener()
+    }
+
+
+    @SuppressLint("MissingPermission")
+    override fun onResume() {
+        super.onResume()
+        this.gpsManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, this.gpsListener)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        this.gpsManager.removeUpdates(this.gpsListener)
+    }
+
+    inner class GPSListener : LocationListener {
+        override fun onLocationChanged(location: Location?) {
+            Log.i("APP_GPS", "${location?.latitude} - ${location?.longitude}")
+        }
+
+        override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
+
+        }
+
+        override fun onProviderDisabled(p0: String?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onProviderEnabled(p0: String?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+    }
+}
+
